a0a1f49f695c92fd03b181fe7f9d362a69817b9e
yogithesymbian <yogirenbox33@gmail.com>
2024-11-29 09:21:59 +0800

    Reference to an enclosing class method cannot be extracted - tried comment on func

--- lib/feed_screen.dart
+++ lib/feed_screen.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_yo_travel/place_model.dart';
+import 'package:flutter_yo_travel/text_theme_x.dart';
 
 class FeedScreen extends StatelessWidget {
   const FeedScreen({
@@ -8,7 +11,118 @@ class FeedScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Yo Travel")),
+      appBar: AppBar(
+        title: const Text("Yo Travel"),
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(CupertinoIcons.search),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(CupertinoIcons.star),
+          )
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: TravelPlace.places.length,
+        itemExtent: 350,
+        physics: const BouncingScrollPhysics(),
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        itemBuilder: (context, index) {
+          final place = TravelPlace.places[index];
+          return Container(
+            margin: const EdgeInsets.only(bottom: 20),
+            padding: const EdgeInsets.all(16.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              image: DecorationImage(
+                image: NetworkImage(place.imagesUrl.first),
+                fit: BoxFit.cover,
+                colorFilter: const ColorFilter.mode(
+                  Colors.black26,
+                  BlendMode.darken,
+                ),
+              ),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    CircleAvatar(
+                      backgroundImage: NetworkImage(place.user.urlPhoto),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          place.user.name,
+                          style: context.bodyText1.copyWith(
+                            color: Colors.white,
+                          ),
+                        ),
+                        Text(
+                          'yesterday at 9.10 pm.',
+                          style: context.bodyText1.copyWith(
+                            color: Colors.white70,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const Spacer(),
+                    const IconButton(
+                      // onPressed: () {},
+                      icon: Icon(
+                        Icons.more_horiz,
+                        color: Colors.white,
+                      ),
+                    )
+                  ],
+                ),
+                const Spacer(),
+                Text(
+                  place.name,
+                  style: context.headline2,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  color: Colors.red,
+                  child: Text(place.statusTag.toString()),
+                ),
+                const Spacer(),
+                Row(
+                  children: [
+                    TextButton.icon(
+                      // onPressed: () {},
+                      style: TextButton.styleFrom(
+                        foregroundColor: Colors.white,
+                        shape: const StadiumBorder(),
+                      ),
+                      icon: const Icon(CupertinoIcons.heart),
+                      label: Text(place.likes.toString()),
+                    ),
+                    TextButton.icon(
+                      // onPressed: () {},
+                      style: TextButton.styleFrom(
+                        foregroundColor: Colors.white,
+                        shape: const StadiumBorder(),
+                      ),
+                      icon: const Icon(CupertinoIcons.reply),
+                      label: Text(place.shared.toString()),
+                    )
+                  ],
+                )
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
