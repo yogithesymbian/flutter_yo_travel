56d340b01a2ab0ed00834337d1184696b525b87d
yogithesymbian <yogirenbox33@gmail.com>
2024-12-02 06:08:08 +0800

    added feed screen

--- lib/feed_screen.dart
+++ lib/feed_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_yo_travel/place_card.dart';
+import 'package:flutter_yo_travel/place_detail_screen.dart';
 import 'package:flutter_yo_travel/place_model.dart';
-import 'package:flutter_yo_travel/text_theme_x.dart';
 import 'package:flutter_yo_travel/travel_navigation_bar.dart';
 
 class FeedScreen extends StatelessWidget {
@@ -33,11 +33,42 @@ class FeedScreen extends StatelessWidget {
         padding: const EdgeInsets.fromLTRB(20, 0, 20, kToolbarHeight + 20),
         itemBuilder: (context, index) {
           final place = TravelPlace.places[index];
-          return PlaceCard(place: place);
+          return PlaceCard(
+            place: place,
+            onPressed: () {
+              Navigator.push(
+                context,
+                PageRouteBuilder(
+                  pageBuilder: (_, animation, __) => FadeTransition(
+                    opacity: animation,
+                    child: PlaceDetailScreen(place: place),
+                  ),
+                ),
+              );
+            },
+          );
         },
       ),
       extendBody: true,
-      bottomNavigationBar: const TravelNavigationBar(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.location_on),
+      ),
+      bottomNavigationBar: TravelNavigationBar(
+        onTap: (index) {},
+        items: [
+          TravelNavigationBarItem(
+            icon: CupertinoIcons.chat_bubble,
+            selectedIcon: CupertinoIcons.chat_bubble_fill,
+          ),
+          TravelNavigationBarItem(
+            icon: CupertinoIcons.square_split_2x2,
+            selectedIcon: CupertinoIcons.square_split_2x2_fill,
+          ),
+        ],
+        currentIndex: 1,
+      ),
     );
   }
 }
---
+++ lib/place_detail_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_yo_travel/place_model.dart';
+
+class PlaceDetailScreen extends StatelessWidget {
+  const PlaceDetailScreen({
+    super.key,
+    required this.place,
+  });
+  final TravelPlace place;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
