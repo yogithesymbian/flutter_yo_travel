b6bbe7f86b7fdbefd4dcdad2750f86ee409e7324
yogithesymbian <yogirenbox33@gmail.com>
2024-12-02 06:44:33 +0800

    feat(detail-screen): added likes and shares container

--- lib/animated_detail_header.dart
+++ lib/animated_detail_header.dart
@@ -1,8 +1,10 @@
 import 'dart:ui';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_yo_travel/place_image_page_view.dart';
 import 'package:flutter_yo_travel/place_model.dart';
+import 'package:flutter_yo_travel/text_theme_x.dart';
 
 class AnimatedDetailHeader extends StatelessWidget {
   const AnimatedDetailHeader({
@@ -23,19 +25,77 @@ class AnimatedDetailHeader extends StatelessWidget {
     return Stack(
       fit: StackFit.expand,
       children: [
-        Positioned.fill(
-          top: (20 + topPadding) * (1 - bottomPercent),
-          bottom: 160 * (1 - bottomPercent),
-          child: Transform.scale(
-            scale: lerpDouble(1, 1.3, bottomPercent),
-            child: PlaceImagePageView(place: place, imagesUrl: imagesUrl),
+        ClipRect(
+          child: Padding(
+            padding: EdgeInsets.only(
+              top: (20 + topPadding) * (1 - bottomPercent),
+              bottom: 160 * (1 - bottomPercent),
+            ),
+            child: Transform.scale(
+              scale: lerpDouble(1, 1.3, bottomPercent),
+              child: PlaceImagePageView(place: place, imagesUrl: imagesUrl),
+            ),
           ),
         ),
         Positioned.fill(
           top: null,
           child: Container(
             height: 140,
-            color: Colors.blue.shade400,
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            decoration: BoxDecoration(
+              color: Colors.blue.shade400,
+              borderRadius: const BorderRadius.vertical(
+                top: Radius.circular(30),
+              ),
+            ),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextButton.icon(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    foregroundColor: Colors.black,
+                    textStyle: context.subtitle1,
+                    shape: const StadiumBorder(),
+                  ),
+                  icon: const Icon(
+                    CupertinoIcons.heart,
+                    size: 26,
+                  ),
+                  label: Text(place.likes.toString()),
+                ),
+                TextButton.icon(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    foregroundColor: Colors.black,
+                    textStyle: context.subtitle1,
+                    shape: const StadiumBorder(),
+                  ),
+                  icon: const Icon(
+                    CupertinoIcons.reply,
+                    size: 26,
+                  ),
+                  label: Text(place.shared.toString()),
+                ),
+                const Spacer(),
+                TextButton.icon(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    foregroundColor: Colors.blue.shade600,
+                    backgroundColor: Colors.blue.shade100,
+                    textStyle: context.subtitle1,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                  ),
+                  icon: const Icon(
+                    Icons.check_circle_outline,
+                    size: 26,
+                  ),
+                  label: const Text('Checkin'),
+                ),
+              ],
+            ),
           ),
         ),
         Positioned.fill(
